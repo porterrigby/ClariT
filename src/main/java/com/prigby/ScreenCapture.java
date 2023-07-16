55b481e2a95765335d707949c07d56eb060ea3e1
Porter Rigby <135761349+porterrigby@users.noreply.github.com>
2023-07-16 03:37:35 -0600

    Fixed some stuff and broke some stuff.

--- src/main/java/com/prigby/ScreenCapture.java
+++ src/main/java/com/prigby/ScreenCapture.java
@@ -4,26 +4,38 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.robot.Robot;
 
-public class ScreenCapture {
+public class ScreenCapture extends Pane {
     private Robot robot;
     private WritableImage capturedScreen;
+    private int mouseX1, mouseX2, mouseY1, mouseY2;
 
 
     public ScreenCapture() throws IOException {
+        this.setPrefSize(2560, 1440);
         robot = new Robot();
     }
 
     public void screenShot() {
-        
-        capturedScreen = robot.getScreenCapture(null, new Rectangle2D(0, 0, 2560, 1440));
+
+        capturedScreen = robot.getScreenCapture(null, new Rectangle2D(mouseX1, mouseY1, mouseX2, mouseY2));
     }
 
     public void saveShot() throws IOException {
         File outputImage = new File("./src/main/java/com/prigby/tmp/screenshot.png");
         ImageIO.write(SwingFXUtils.fromFXImage(capturedScreen, null), "png", outputImage);    
+    }
+
+
+    //TODO fix this shitssssVVV
+    private void getMouseCoordinates(MouseEvent event) {
+        mouseX1 = (int)(event.getX());
+        mouseY1 = (int)(event.getY());
     }
 }
