2bc58b9bcd8a60760ba1b9dae33e51be9d9fd404
Porter Rigby <135761349+porterrigby@users.noreply.github.com>
2023-07-16 11:19:59 -0600

    Fixed lambda source issue in pom file.

--- src/main/java/com/prigby/ScreenCapture.java
+++ src/main/java/com/prigby/ScreenCapture.java
@@ -15,27 +15,42 @@ public class ScreenCapture extends Pane {
     private Robot robot;
     private WritableImage capturedScreen;
     private int mouseX1, mouseX2, mouseY1, mouseY2;
+    private boolean hasBeenCropped, hasBeenShot;
 
 
     public ScreenCapture() throws IOException {
         this.setPrefSize(2560, 1440);
         robot = new Robot();
+        hasBeenCropped = hasBeenShot = false;
+    }
+
+    public void cropScreen() {
+        EventHandler<MouseEvent> mouseClickHandler = (MouseEvent event) -> {handleMouseClick(event);};
+        EventHandler<MouseEvent> mouseDragHandler = (MouseEvent event) -> {handleMouseDrag(event);};
+        this.setOnMousePressed(mouseClickHandler);
+        this.setOnMouseReleased(mouseDragHandler);
     }
 
     public void screenShot() {
-
+        if (!hasBeenCropped) {throw new RuntimeException("No screen selection was made.");}
         capturedScreen = robot.getScreenCapture(null, new Rectangle2D(mouseX1, mouseY1, mouseX2, mouseY2));
     }
 
     public void saveShot() throws IOException {
+        if (!hasBeenShot) {throw new RuntimeException("No screenshot was captured.");}
         File outputImage = new File("./src/main/java/com/prigby/tmp/screenshot.png");
         ImageIO.write(SwingFXUtils.fromFXImage(capturedScreen, null), "png", outputImage);    
     }
 
 
     //TODO fix this shitssssVVV
-    private void getMouseCoordinates(MouseEvent event) {
+    private void handleMouseClick(MouseEvent event) {
         mouseX1 = (int)(event.getX());
         mouseY1 = (int)(event.getY());
+    }
+
+    private void handleMouseDrag(MouseEvent event) {
+        mouseX2 = (int)(event.getX());
+        mouseY2 = (int)(event.getY());
     }
 }
