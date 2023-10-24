5299a5b72e3a5ef477db7b267e715a2e383b9bd9
porterrigby <prigby.a@gmail.com>
2023-10-23 19:59:26 -0600

    Changed mouse y offset value to 16 from 17

--- src/main/java/com/prigby/GrabImage.java
+++ src/main/java/com/prigby/GrabImage.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class GrabImage extends Application {
-    private final int MOUSE_Y_OFFSET = 17;
+    private final int MOUSE_Y_OFFSET = 16;
     private boolean mousePressed;
     private double x1, y1, x2, y2;
     private ScreenCapture screenCapture;
